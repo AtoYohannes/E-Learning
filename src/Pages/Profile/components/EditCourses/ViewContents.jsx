46410ac6c66555c25c8734b40d18a7dd9b21580e
Yohannes Berhanu <Johnlights51@gmail.com>
2021-05-10 22:45:03 +0300

    Removed Unused Variables

--- src/Pages/Profile/components/EditCourses/ViewContents.jsx
+++ src/Pages/Profile/components/EditCourses/ViewContents.jsx
@@ -1,39 +1,29 @@
-import Joi from "joi-browser";
+import CustomTable from "common/table";
+import { getContentsFromChapter } from "helpers/customResolvers";
 import React from "react";
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Col,
-  Form,
-  Nav,
-  NavItem,
-  NavLink,
-  Row,
-  TabContent,
-  TabPane,
-} from "reactstrap";
-import ParentForm from "../../../../common/form";
-import CustomTable from "common/table"
-import { getContentsFromChapter } from "helpers/customResolvers"
+import { Card, CardBody } from "reactstrap";
 
 const ViewContents = ({ data, options }) => {
-  console.log("contents", options.contents)
+  console.log("contents", options.contents);
   const columns = [
     { path: "title", label: "Title" },
     { path: "contentType", label: "Content Type" },
     { path: "isMandatory", label: "Is Mandatory" },
     { path: "timeRequiredInMinutes", label: "Time Required" },
-  ]
-  console.log("here", data)
+  ];
+  console.log("here", data);
 
   return (
     <Card className="border-0 bg-background">
       <CardBody className="bg-background ">
-      <CustomTable data={getContentsFromChapter(data.chapterID, options.contents)} columns={columns} title="Contents" />
+        <CustomTable
+          data={getContentsFromChapter(data.chapterID, options.contents)}
+          columns={columns}
+          title="Contents"
+        />
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
 export default ViewContents;
--- src/Pages/Profile/components/EditCourses/index.jsx
+++ src/Pages/Profile/components/EditCourses/index.jsx
@@ -1,64 +1,83 @@
-import React, { useReducer, useEffect, useState } from "react"
-import CommonModals from "common/CommonModal";
-import { initialState, reducer, _toggle } from "common/ModalOptions";
-import { Card, Col, Row, Button } from "reactstrap";
-import CoursesCard from "../../../../Components/Card/CoursesCard";
-import Courses from "./Courses"
-import reduxStatus from "constants/reduxStatus"
-import { connect } from "react-redux"
-import { selectAddStatus } from "store/States/UnCompletedCourses"
-import { selectAddStatus as selectChapterAddStatus, AddChapter, Add, selectChapters } from "store/States/Chapters/"
+import { initialState, reducer } from "common/ModalOptions";
+import reduxStatus from "constants/reduxStatus";
+import React, { useEffect, useReducer, useState } from "react";
+import { connect } from "react-redux";
 import {
-  selectAddStatus as selectContentAddStatus, AddContent, Add as addContent,
-  selectContents, Edit, VerifyCourse, selectEditStatus
-} from "store/States/Contents"
-import { FetchCourses, Fetch, selectCourses } from "store/States/Courses/"
+  Add,
+  AddChapter,
+  selectAddStatus as selectChapterAddStatus,
+  selectChapters,
+} from "store/States/Chapters/";
+import {
+  Add as addContent,
+  AddContent,
+  Edit,
+  selectAddStatus as selectContentAddStatus,
+  selectContents,
+  selectEditStatus,
+  VerifyCourse,
+} from "store/States/Contents";
+import { Fetch, FetchCourses, selectCourses } from "store/States/Courses/";
+import { selectAddStatus } from "store/States/UnCompletedCourses";
+import Courses from "./Courses";
 
 const UncompletedCoures = ({
-  unCompletedCourses, addStatus, addContentStatus, postChapter, postContent, addChapterStatus,
-  chapters, contents, editStatus, verifyCourse, fetchCourses
+  unCompletedCourses,
+  addStatus,
+  addContentStatus,
+  postChapter,
+  postContent,
+  addChapterStatus,
+  chapters,
+  contents,
+  editStatus,
+  verifyCourse,
+  fetchCourses,
 }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [fetchLock, setFetchLock] = useState(true);
   const [addLock, setAddLock] = useState(true);
-  const [addChapterLock, setAddChapterLock] = useState(true)
-  const [addContentLock, setAddContentLock] = useState(true)
+  const [addChapterLock, setAddChapterLock] = useState(true);
+  const [addContentLock, setAddContentLock] = useState(true);
   const [editLock, setEditLock] = useState(true);
   const [deleteLock, setDeleteLock] = useState(true);
-  console.log("course", unCompletedCourses)
+  console.log("course", unCompletedCourses);
   const _addCourse = () => {
-    setAddLock(false)
-  }
+    setAddLock(false);
+  };
 
   const _postChapter = (data) => {
-    setAddChapterLock(false)
-    postChapter(data)
-  }
+    setAddChapterLock(false);
+    postChapter(data);
+  };
 
   const _postContent = (data) => {
-    setAddContentLock(false)
-    postContent(data)
-  }
+    setAddContentLock(false);
+    postContent(data);
+  };
 
   const _verifyCourse = (id) => {
-    setEditLock(false)
-    verifyCourse(id)
-  }
-
+    setEditLock(false);
+    verifyCourse(id);
+  };
 
   useEffect(() => {
     if (editStatus.status === reduxStatus.success && !editLock) {
-      setEditLock(true)
-      fetchCourses()
+      setEditLock(true);
+      fetchCourses();
     }
-  }, [editStatus, editLock, setEditLock, fetchCourses])
+  }, [editStatus, editLock, setEditLock, fetchCourses]);
 
   return (
     <Courses
       doneAdd={addStatus.status === reduxStatus.success && !addLock}
       doneEdit={editStatus.status === reduxStatus.success && !editLock}
-      doneAddChapter={addChapterStatus.status === reduxStatus.success && !addChapterLock}
-      doneAddContent={addContentStatus.status === reduxStatus.success && !addContentLock}
+      doneAddChapter={
+        addChapterStatus.status === reduxStatus.success && !addChapterLock
+      }
+      doneAddContent={
+        addContentStatus.status === reduxStatus.success && !addContentLock
+      }
       addCourse={_addCourse}
       postChapter={_postChapter}
       postContent={_postContent}
@@ -68,24 +87,24 @@ const UncompletedCoures = ({
       contents={contents}
     />
     // <Courses />
-  )
-}
+  );
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   unCompletedCourses: selectCourses(state),
   addStatus: selectAddStatus(state),
   addChapterStatus: selectChapterAddStatus(state),
   addContentStatus: selectContentAddStatus(state),
   chapters: selectChapters(state),
   contents: selectContents(state),
-  editStatus: selectEditStatus(state)
-})
+  editStatus: selectEditStatus(state),
+});
 
-const mapDispatchToProps = dispatch => ({
-  postChapter: data => dispatch(Add(AddChapter(data))),
-  postContent: data => dispatch(addContent(AddContent(data))),
-  verifyCourse: id => dispatch(Edit(VerifyCourse(id))),
-  fetchCourses: () => dispatch(Fetch(FetchCourses()))
-})
+const mapDispatchToProps = (dispatch) => ({
+  postChapter: (data) => dispatch(Add(AddChapter(data))),
+  postContent: (data) => dispatch(addContent(AddContent(data))),
+  verifyCourse: (id) => dispatch(Edit(VerifyCourse(id))),
+  fetchCourses: () => dispatch(Fetch(FetchCourses())),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(UncompletedCoures)
+export default connect(mapStateToProps, mapDispatchToProps)(UncompletedCoures);
